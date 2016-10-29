f6aebcb4a801d54b10bd403303f003e0266e55d0
Elliott Pardee <me@elliottpardee.me>
2016-10-29 17:33:24 -0400

    Added some backend functions, commented most of the code.

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var person = require('./person');
 var sha512 = require('js-sha512').sha512;
 var bodyParser = require('body-parser');
 var sanitizer = require('sanitizer');
@@ -14,41 +13,78 @@ router.use(bodyParser.json());
 
 // backend functions
 function checkIfExists(username, email) {
-    // ------ TODO ------
-    // check if a username and/or email already exists in the database
-    // returns: [boolean, "username" || "email" || "email and username" ]
+    // checks the database for any existing emails or usernames
+    // returns: [boolean, "username" || "email" || "email and username" || "null"]
+    var result = [false, "null"];
+
+    db.find({ "username": username }, function (err, docs) {
+        if (docs.length == 1) {
+            result[0] = true;
+            result[1] = "username";
+        }
+    });
+
+    db.find({ "email": email }, function (err, docs) {
+        if (docs.length == 1) {
+            if (result[0] == true) {
+                result[1] = "email and username";
+            } else {
+                result[0] = true;
+                result[1] = "email";
+            }
+        }
+    });
+
+    return result;
 }
 
 function isReserved(username) {
-    // ------ TODO ------
-    // check if the username isn't already reserved by the Retejo Team
-    // examples: "evildea", "admin", "owner", etc...
+    // pretty self explanitory, checks if the username is reserved
     // returns: boolean
+    var reservedUsernames = [ "evildea", "zamenhof", "admin", "owner", "staff", "team", "vypr", "itwango", "stupiddroid", "moderator", "amuzulo"];
+
+    for (var i = 0; i < reservedUsernames.length; i++) {
+        if (username == reservedUsernames[i]) {
+            return true;
+        }
+    }
 }
 
 function databaseInsert(username, password, email, joinDate) {
-    // ------ TODO ------
     // inserts the data into the database, no kidding, right?
     // returns: boolean
 
-    // var data = {"username": username, "password": password, "email": email, "joinDate": joinDate};
+    // create a json object based on the values
+    var data = {"username": username, "password": password, "email": email, "joinDate": joinDate};
+
+    db.insert(data, function (err, newDocument) {
+        if (err != null) {
+            return false;
+        }
+
+        return true;
+    });
 }
 
 // routes
 router.post('/signup', function (req, res) {
         var data = req.body;
 
+        // makes sure that they match, so that way people don't mess up accidentally
         if (data.password != data.confirmpass) { res.send("passwords don't match"); return; }
         if (data.email != data.confirmemail) { res.send("emails don't match"); return; }
 
+        // escape every input, hash the password (with a random salt), and create a join date
         var username = sanitizer.escape(data.username);
         var password = sha512(sanitizer.escape(data.password) + crypto.randomBytes(16).toString("hex"));
         var email = sanitizer.escape(data.email);
         var joinDateObject = new Date();
         var joinDate = joinDateObject.getTime();
 
-        var existenceStatus = checkIfExists(username, email)[0];
+        // checks the database if the email and/or the username already exists
+        var existenceStatus = checkIfExists(username, email);
 
+        // if cases handling what happens as a result of the data;
         if (!isReserved(username)) {
             res.send("this username is reserved, please contact team@retejo.me for more information");
         } else if (existenceStatus[0]) {
@@ -63,6 +99,7 @@ router.post('/signup', function (req, res) {
 });
 
 router.get('/id/:id', function (req, res) {
+    // an api call for pulling the data of the user, will be used later, ignore for now
     res.set('Content-Type', 'application/json');
 
     db.find({ _id: req.params.id }, function (err, result) {
--- start.js
+++ start.js
@@ -12,16 +12,18 @@ var app = express();
 var host = "127.0.0.1";
 var port = 5555;
 
-app.use('/api', api);
+app.use('/api', api); // make sure the api routes are hooked up and working at /api
 
+// ensure that handlebars is the view engine on express' end
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/v');
 hbs.registerPartials(__dirname + '/v/part');
 
+// load the assets into their proper folder so the templates run smoothly
 app.use('/js',express.static(__dirname + '/assets/js'));
 app.use('/css',express.static(__dirname + '/assets/css'));
 
-// frontend routes (currently commented out, for now...)
+// frontend routes
 /*
         app.get('/profile/:id', function (req, res) {
             fetch("http://" + host + ":" + port + "/api/id/" + req.params.id)
@@ -32,10 +34,11 @@ app.use('/css',express.static(__dirname + '/assets/css'));
         });
 */
 
-app.get('/create', function (req, res) {
-    res.render('create', {pagename: "Create"});
+app.get('/signup', function (req, res) {
+    res.render('signup', {pagename: "Sign up"});
 })
 
+// handle CLI arguments, and respond accordingly
 commander.version('1.0.0')
          .option('-h, --host', 'set hostname [default: 127.0.0.1]')
          .option('-p, --port', 'set port [default: 5555]')
@@ -49,6 +52,7 @@ if (commander.port) {
     port = commander.port;
 }
 
+// houston, we have lift off
 app.listen(port, host, function () {
     console.log(chalk.bold.cyan('Server started on ' +
                                 chalk.bold.green(host + ':' + port) + '.'));
