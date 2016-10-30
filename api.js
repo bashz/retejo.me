496f05223586c1454ccef992d9f4a2e56b7d3434
Elliott Pardee <me@elliottpardee.me>
2016-10-30 00:29:18 -0400

    Ensuring things function properly

--- api.js
+++ api.js
@@ -16,7 +16,7 @@ var captchaSiteKey = process.env.CAPTCHA_SITE;
 var apiKey = process.env.API_KEY;
 
 // check if the api key is valid, otherwise deny ANY functionality
-if (sha512(apiKey) != "0b8d819370a076884785850fdb5278b17558b8f83efa7f989f5ccbc75e937e81ded485c1f5538e353daeef12727371228e370f4fe5628c431b4210e472ed2ca1") {
+if (sha512(apiKey + '') != "0b8d819370a076884785850fdb5278b17558b8f83efa7f989f5ccbc75e937e81ded485c1f5538e353daeef12727371228e370f4fe5628c431b4210e472ed2ca1") {
     return { error: 401, text: "you shall not use this api" };
 }
 
--- start.js
+++ start.js
@@ -5,6 +5,7 @@ var app = require('./app');
 // settings
 var host = "127.0.0.1";
 var port = 5555;
+
 // handle CLI arguments, and respond accordingly
 commander.version('1.0.0')
          .option('-h, --host', 'set hostname [default: 127.0.0.1]')
